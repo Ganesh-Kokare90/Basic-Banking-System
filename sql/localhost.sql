5fb9281606cd4ed5af3577e94a14a8b977c1f804
Ganesh-Kokare90 <94785707+Ganesh-Kokare90@users.noreply.github.com>
2021-11-21 19:19:41 +0530

    Add files via upload

--- sql/localhost.sql
+++ sql/localhost.sql
@@ -1,1 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 09, 2021 at 05:56 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.23
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: ``90_Ganesh`
+--
+CREATE DATABASE IF NOT EXISTS `90_Ganesh` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `90_Ganesh`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(2, 'Omkar', 'Rahul', 9, '2021-04-08 18:08:56'),
+(3, 'Rushabh', 'Aishwarya', 100, '2021-04-08 14:55:43'),
+(4, 'Pratik', 'Tushar', 1, '2021-04-08 14:58:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Rushabh', 'rushabhshaha57@gmail.com', 50000),
+(2, 'Omkar', 'Omkar85@gmail.com', 30000),
+(3, 'Aishwarya', 'Aishwaryashah@gmail.com', 39900),
+(4, 'Pratik', 'pratikkhomane5757@gmail.com', 10000),
+(5, 'Tushar', 'tusharborawake2001@gmail.com', 40000),
+(6, 'Rahul', 'rahulchaudhari21@gmail.com', 19990),
+(7, 'Shubham', 'Shubham2000@gmail.com', 50009),
+(8, 'Ganesh', 'ganeshkokare9090@gmail.com', 40100),
+(9, 'Mayuri', 'mayuri88@gmail.com', 30000),
+(10, 'Aniket', 'aniket34@gmail.com', 50001);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
